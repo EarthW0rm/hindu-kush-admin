c44f93ccf3ba671b717c8ba2411030c5f9b757bd
EarthW0rm <marco.a.faustino@gmail.com>
2018-03-28 00:29:41 -0300

    Configuracoes iniciais com a cópia do html index executada

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'style.css'
@@ -32,9 +33,22 @@ module.exports = {
                     use: ['css-loader', 'sass-loader']
                 })
             }
+            ,{
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader",
+                    options: { minimize: true }
+                  }
+                ]
+            }
         ]
     }
     , plugins: [
         extractPlugin
+        ,new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
     ]
 };
